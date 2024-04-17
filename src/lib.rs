b0b9629637b80d383f9a4409a92b13e3eb404ad5
mcbegamerxx954 <154642722+mcbegamerxx954@users.noreply.github.com>
2024-04-17 00:00:00 +0000

    fix: Fixed some ub and glitches

--- src/lib.rs
+++ src/lib.rs
@@ -11,19 +11,30 @@ use std::{fs, thread};
 static SHADER_PATHS: Lazy<Mutex<HashMap<OsString, PathBuf>>> =
     Lazy::new(|| Mutex::new(HashMap::new()));
 
+// A quick startpoint for the library, mostly there because
+// unwinding up here is ub, + give a good panic message
 #[ctor::ctor]
-fn start_lib() {
-    std::panic::catch_unwind(|| {
-        platform::setup_logging();
-        startup();
-    });
+fn ctor() {
+    std::thread::spawn(safe_setup);
 }
-
-fn startup() {
-    log::info!("Starting up!");
+// Make sure that ub cant happen when unwinding
+// and provide usefull info
+fn safe_setup() {
+    platform::setup_logging();
     std::panic::set_hook(Box::new(move |panic_info| {
         log::error!("Thread crashed: {}", panic_info);
     }));
+    let start = std::panic::catch_unwind(|| {
+        startup();
+    });
+    if let Err(e) = start {
+        if let Ok(err) = e.downcast::<String>() {
+            log::error!("Thread crash, error: {err}");
+        }
+    }
+}
+fn startup() {
+    log::info!("Starting up!");
     platform::setup_hooks().unwrap();
     log::info!("Finished hooking..");
     let mut path = platform::get_path();
@@ -37,8 +48,5 @@ fn startup() {
         }
     }
     log::debug!("path is : {:#?}", &path);
-    let _handler = thread::spawn(|| {
-        log::info!("Hello from thread");
-        common::setup_json_watcher(path);
-    });
+    common::setup_json_watcher(path);
 }
